f1f9d9821fa72b3fa384736c25befd84f6f07e05
Joel Palmer <joelpalmer@outlook.com>
2015-06-15 10:57:34 -0500

    Added Number prototype extension --> toBinary()

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //data structure run and test script
 
-//initial stack
+//initial stack running code
 var stack = new JSStack();
 console.log(stack.isEmpty());
 stack.push(7);
@@ -13,3 +13,25 @@ stack.push(3);
 stack.pop();
 console.log(stack.count());
 stack.print();
+
+//use stack to convert a number to binary string
+Number.prototype.toBinary = function () {
+    var remStack = new JSStack(),
+        rem,
+        num = this,
+        binaryString = '';
+
+    while (num > 0) { //{1}
+        rem = Math.floor(num % 2);
+        remStack.push(rem);
+        num = Math.floor(num / 2);
+    }
+
+    while (!remStack.isEmpty()) {
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+var theNumber = 12;
+console.log(theNumber.toBinary());
